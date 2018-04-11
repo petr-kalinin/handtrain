0c8c6b235c47f961403bf360db7506682b63e0f0
Petr Kalinin <petr@kalinin.nnov.ru>
2018-04-11 21:44:46 +0300

    Added letter training

--- runTraining.py
+++ runTraining.py
@@ -18,6 +18,7 @@ reqTime = 1000
 # drawer = DummyDrawer()
 drawer = PygameDrawer("car_150px.png", bgColor=(255, 255, 255), activeColor=(128, 128, 128), okColor=(0, 255, 0))
 cup_drawer = PygameDrawer("cup_500px.png", bgColor=(255, 255, 255), activeColor=(128, 128, 128), okColor=(0, 255, 0))
+letter_drawer = PygameDrawer("letter_150px.png", bgColor=(255, 255, 255), activeColor=(128, 128, 128), okColor=(0, 255, 0))
 
 #joy1 = JoystickImitator(0, 0.95, 0, 0.95)
 #joy2 = JoystickImitator(0, 0.95, 0, 0.95)
@@ -43,11 +44,15 @@ sequence.append(AlignmentTraining(drawer,
                                   (0.95, 0.15, 0.15, 0.95),
                                   (0.05, 0.85, 0.85, 0.05)))
 """
+"""
 sequence.append(CupTraining(cup_drawer,
                             (0.7, 0.5),
                             (0.3, 0.5),
                             math.pi/4))
-
+"""
+sequence.append(AlignmentTraining(letter_drawer,
+                                  (0.95, 0.15, 0.15, 0.95),
+                                  (0.05, 0.85, 0.85, 0.05)))
 
 sequence.run()
 print(sequence.resDelta)
